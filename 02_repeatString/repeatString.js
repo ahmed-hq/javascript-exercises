503c585e1f0c2245b0f4a29bcb5f5eadbcad9df1
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-02-06 15:10:31 +0200

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let string = '';  
+    for (i=0; i < num; i++){
+        string += str
+    }
+    return string;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
